655ef24cb13b40abe09011b3d3fafbdfb3877c04
zhouzhi <zhouzhi073859@outlook.com>
2019-08-12 17:38:41 +0800

    feat: 添加两数之和与测试文件

--- leetcode/addTwoNumbers.js
+++ leetcode/addTwoNumbers.js
@@ -7,6 +7,17 @@ function ListNode(val) {
   this.val = val;
   this.next = null;
 }
-const addTwoNumbers = function(l1, l2) {
-
+const addTwoNumbers = function(l1, l2, isExceed10 = false) {
+  let node = null;
+  if (l1 || l2) {
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+    const val = isExceed10 ? val1 + val2 + 1 : val1 + val2;
+    node = new ListNode(val % 10);
+    node.next = addTwoNumbers(l1 ? l1.next : null, l2 ? l2.next : null, val >= 10);
+  } else if (isExceed10) {
+    node = new ListNode(1);
+    node.next = null;
+  }
+  return node;
 };
